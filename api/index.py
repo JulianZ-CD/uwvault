9030f93b3afef276ef7cdefdbed43212b1f79606
JuDman <41619294+JulianZ-CD@users.noreply.github.com>
2025-01-22 17:14:52 -0500

    feat: integrate todo router into FastAPI application

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from api.routers.todo_router import router as todo_router
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+
+app.include_router(todo_router)
 
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
