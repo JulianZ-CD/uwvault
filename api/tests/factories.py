2cf73463d8da92b6b1a7a1528a901e1738e42f52
JuDman <41619294+JulianZ-CD@users.noreply.github.com>
2025-01-23 10:15:33 -0500

    feat: introduce consistent datetime formatting in Todo factories and add TodoUpdateFactory

--- api/tests/factories.py
+++ api/tests/factories.py
@@ -2,6 +2,10 @@ import factory
 from datetime import datetime
 from api.models.todo import Todo, TodoCreate, TodoUpdate
 
+def format_datetime():
+    """Helper function to format datetime consistently"""
+    return datetime.now().isoformat() + 'Z'
+
 class TodoFactory(factory.Factory):
     class Meta:
         model = Todo
@@ -11,9 +15,10 @@ class TodoFactory(factory.Factory):
     description = factory.Sequence(lambda n: f'Test Description {n}')
     is_completed = False
     priority = 1
-    due_date = factory.LazyFunction(datetime.now)
-    created_at = factory.LazyFunction(datetime.now)
-    updated_at = factory.LazyFunction(datetime.now)
+    due_date = factory.LazyFunction(format_datetime)
+    created_at = factory.LazyFunction(format_datetime)
+    updated_at = factory.LazyFunction(format_datetime)
+
 
 class TodoCreateFactory(factory.Factory):
     class Meta:
@@ -23,4 +28,17 @@ class TodoCreateFactory(factory.Factory):
     description = factory.Sequence(lambda n: f'New Description {n}')
     is_completed = False
     priority = 1
-    due_date = factory.LazyFunction(datetime.now) 
+    due_date = factory.LazyFunction(format_datetime)
+
+
+class TodoUpdateFactory(factory.Factory):
+    class Meta:
+        model = TodoUpdate
+
+    title = factory.Sequence(lambda n: f'Updated Todo {n}')
+    description = factory.Sequence(lambda n: f'Updated Description {n}')
+    # Alternates between True and False
+    is_completed = factory.Sequence(lambda n: n % 2 == 0)
+    # Generates a priority between 1 and 5
+    priority = factory.Sequence(lambda n: (n % 5) + 1)
+    due_date = factory.LazyFunction(format_datetime)
