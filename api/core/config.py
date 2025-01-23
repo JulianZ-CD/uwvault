2f6151707ac90f53a27c9f128c4d85bc55b9fe74
JuDman <41619294+JulianZ-CD@users.noreply.github.com>
2025-01-22 20:19:06 -0500

    refactor: update ConfigDict configuration management in Settings class

--- api/core/config.py
+++ api/core/config.py
@@ -1,12 +1,15 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
+from pydantic import ConfigDict
 
 class Settings(BaseSettings):
     SUPABASE_URL: str
     SUPABASE_KEY: str
 
-    class Config:
-        env_file = ".env.dev"
+    model_config = ConfigDict(
+        env_file=".env.dev"
+    )
+
 
 @lru_cache()
 def get_settings():
