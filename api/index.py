409424dfdcce4fa15d4b35ca0d34d83226c41034
Ziyu <ziyuwang123456@gmail.com>
2025-01-30 18:01:43 -0500

    chore: remove hardcoded CORS configuration

--- api/index.py
+++ api/index.py
@@ -4,15 +4,6 @@ from api.routers.api_router import api_router
 
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
-# CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 app.include_router(api_router)
 
