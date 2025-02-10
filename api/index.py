979187ef532dbc9d6447f890960cc6b90c113b10
Ziyu <ziyuwang123456@gmail.com>
2025-02-10 14:29:01 -0500

    feat: add todo router to FastAPI application

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from api.routers.auth_router import router as auth_router
+from api.routers.todo_router import router as todo_router
 
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
 
 app.include_router(auth_router, prefix="/api/py")
+app.include_router(todo_router, prefix="/api/py")
 
 
 @app.get("/api/py/helloFastApi")
